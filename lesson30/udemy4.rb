0814957d71cea3526b00da46062ba7302772b725
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-18 12:06:36 +0500

    refactoring done

--- lesson30/udemy4.rb
+++ lesson30/udemy4.rb
@@ -6,23 +6,19 @@ users = [
           { username: "heisenberg", password: "password5" }
         ]
 
-login_success = false
+user_info = {}
 
 puts 'Welcome to the authenticator'
 20.times { print "-" }
 puts
 puts 'This program will take input from the user and compare password'
 print 'Username: '
-username = gets.chomp
+user_info[:username] = gets.chomp
 print 'Password: '
-password = gets.chomp
+user_info[:password] = gets.chomp
 
 users.each do |user|
-  if username == user[:username] && password == user[:password]
-    login_success = true
+  if user == user_info
+    puts user_info
   end
-end
-
-if login_success == true
-  
 end
