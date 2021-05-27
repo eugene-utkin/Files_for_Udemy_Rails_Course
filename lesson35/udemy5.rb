4715b38596f5105bf75bd3161e9deb233d80941f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-27 11:24:10 +0500

    lesson 35 - loop with question created

---
+++ lesson35/udemy5.rb
@@ -0,0 +1,29 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+# Get city names from the hash
+def get_city_names(somehash)
+# Write code here
+end
+ 
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+# Write code here
+end
+ 
+# Execution flow
+loop do
+  puts 'Do you want to lookup an area code based on a city name?(Y/N)'
+  answer = gets.chomp.downcase
+  break if answer != "y"
+end
