6bd1c87b6b65586e790f25f649e96e32eb9bdde7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-12 10:46:29 +0500

    rollbacked

--- lesson76/alpha-blog/db/schema.rb
+++ lesson76/alpha-blog/db/schema.rb
@@ -10,10 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_062927) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
