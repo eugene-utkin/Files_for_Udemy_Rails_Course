508d764169f1f9ecb8fe019a5536942f899f4979
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-12 10:48:18 +0500

    create_articles migration ran again

--- lesson76/alpha-blog/db/schema.rb
+++ lesson76/alpha-blog/db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_11_062927) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+  end
 
 end
