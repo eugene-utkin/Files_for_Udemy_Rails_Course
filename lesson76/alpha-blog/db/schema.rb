bae71e27b5f015f554f45968931dbca3436546f6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-13 11:09:45 +0500

    migration ran

--- lesson76/alpha-blog/db/schema.rb
+++ lesson76/alpha-blog/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_062927) do
+ActiveRecord::Schema.define(version: 2021_08_12_055120) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
