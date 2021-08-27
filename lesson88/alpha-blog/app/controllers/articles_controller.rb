0809bc11ea6ca559abb9cf7fa66ee2adf57d8668
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-27 09:59:10 +0500

    start of Lesson 88

---
+++ lesson88/alpha-blog/app/controllers/articles_controller.rb
@@ -0,0 +1,12 @@
+class ArticlesController < ApplicationController
+
+  def show
+    byebug
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+end
