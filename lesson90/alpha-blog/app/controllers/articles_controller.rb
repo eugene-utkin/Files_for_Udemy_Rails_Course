1400a8e9def6ab2f70ea319556c60c2126800401
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-09-01 11:21:08 +0500

    redirect added to article create method

--- lesson90/alpha-blog/app/controllers/articles_controller.rb
+++ lesson90/alpha-blog/app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     @article.save
+    redirect_to article_path(@article)
   end
 
 end
