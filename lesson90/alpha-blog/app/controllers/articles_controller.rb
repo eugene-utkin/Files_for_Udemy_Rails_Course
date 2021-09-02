6aaaed80778f5a3d878d5d067c12c4f36bda6e6e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-09-02 10:58:26 +0500

    short form redirect_to @article used

--- lesson90/alpha-blog/app/controllers/articles_controller.rb
+++ lesson90/alpha-blog/app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     @article.save
-    redirect_to article_path(@article)
+    redirect_to @article
   end
 
 end
