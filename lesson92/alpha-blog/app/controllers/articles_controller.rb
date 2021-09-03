f4ea65f0d6be82bd31d40e3bece2bf1ac1f70466
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-09-03 10:37:27 +0500

    if @article.save added to create method for articles

--- lesson92/alpha-blog/app/controllers/articles_controller.rb
+++ lesson92/alpha-blog/app/controllers/articles_controller.rb
@@ -14,8 +14,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+
+    end
   end
 
 end
