3dd354a8e9275d76174526512369286abff0d84a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-09-05 18:24:12 +0500

    flash[:notice] created fo successfull articel creation

--- lesson92/alpha-blog/app/controllers/articles_controller.rb
+++ lesson92/alpha-blog/app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
+      flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new'
