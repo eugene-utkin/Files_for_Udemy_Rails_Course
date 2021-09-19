040f825be98a43dfb76adb6be33707338ba119df
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-09-19 18:45:02 +0500

    article_params method added to articles controller

--- lesson100/alpha-blog/app/controllers/articles_controller.rb
+++ lesson100/alpha-blog/app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new()
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-
+    params.require(:article).permit(:title, :description)
   end
 
 end
