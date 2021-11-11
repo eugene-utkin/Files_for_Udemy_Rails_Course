c89f5181c75fb22fac24eb7d408387f47bb7c14f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-11-11 10:55:04 +0500

    User scaffold generated

--- lesson125/test_app_6/config/routes.rb
+++ lesson125/test_app_6/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 end
