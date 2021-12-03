c4ab0d146e2b89d657bd721056ea939038a8d0fc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-03 23:09:15 +0500

    if save else added to create user method

--- lesson140/alpha-blog/app/controllers/users_controller.rb
+++ lesson140/alpha-blog/app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
   end
 
 end
