ff0b57913e731e398c6a63ab0ef2f8066cd56e2c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-06 19:37:08 +0500

    username added to signup message

--- lesson140/alpha-blog/app/controllers/users_controller.rb
+++ lesson140/alpha-blog/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog, you have successfully signed up"
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
