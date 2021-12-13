48f4aa477f79b43395754acca4d3018c9a856ee0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-13 17:49:58 +0500

    gravatar_for helper modified

--- lesson144/alpha-blog/app/helpers/application_helper.rb
+++ lesson144/alpha-blog/app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 
   def gravatar_for(user)
-
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+    image_tag(gravatar_url, alt: user.username)
   end
-  
+
 end
