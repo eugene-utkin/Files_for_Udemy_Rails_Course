c02b80b09c3be462f7aa8b71e7d821343a45a34b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-14 17:10:49 +0500

    typo fixed

--- lesson144/alpha-blog/app/helpers/application_helper.rb
+++ lesson144/alpha-blog/app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
   def gravatar_for(user, options = { size: 80})
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
-    gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username)
   end
 
