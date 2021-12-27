b27074b131887d35a03ea029fb3a2ff07e39a3ce
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-27 20:55:38 +0500

    user password check added to session create action

--- lesson152/alpha-blog/app/controllers/sessions_controller.rb
+++ lesson152/alpha-blog/app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    end
   end
 
   def destroy
