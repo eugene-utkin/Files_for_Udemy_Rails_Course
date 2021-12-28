b721597381a27d456823d368501d54f62fbaea86
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-28 20:33:59 +0500

    session creates after successfull log in

--- lesson152/alpha-blog/app/controllers/sessions_controller.rb
+++ lesson152/alpha-blog/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id]
+      session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
