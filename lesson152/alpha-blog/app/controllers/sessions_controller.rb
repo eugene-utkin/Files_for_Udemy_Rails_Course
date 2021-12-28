4416040cbf69fe57d80e99d8612ef2cede054457
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-28 20:29:45 +0500

    redirect_to user added for successfull log in

--- lesson152/alpha-blog/app/controllers/sessions_controller.rb
+++ lesson152/alpha-blog/app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
         flash[:notice] = "Logged in successfully"
+        redirect_to user
     else
       flash.now[:alert] = "There was something wrong with your login details"
       render 'new'
