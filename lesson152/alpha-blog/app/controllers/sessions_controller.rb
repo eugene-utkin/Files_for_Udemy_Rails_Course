45935159f5cbc934eaaecddb3e42d656598943ab
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-28 20:28:29 +0500

    flash notice message added for successfull log in

--- lesson152/alpha-blog/app/controllers/sessions_controller.rb
+++ lesson152/alpha-blog/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-        flash[:notice]
+        flash[:notice] = "Logged in successfully"
     else
       flash.now[:alert] = "There was something wrong with your login details"
       render 'new'
