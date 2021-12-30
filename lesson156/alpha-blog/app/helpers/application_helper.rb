549cfe04512924691903f7407440562b7f69c963
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-30 18:03:59 +0500

    current_user method refactored

--- lesson156/alpha-blog/app/helpers/application_helper.rb
+++ lesson156/alpha-blog/app/helpers/application_helper.rb
@@ -9,9 +9,7 @@ module ApplicationHelper
   end
 
   def current_user
-    if session[:user_id]
-      User.find(session[:user_id])
-    end
+    User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
