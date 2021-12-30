0c44bf02a6daaab0e9d2a5aa34ca73fdf7c0b6cc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-30 18:25:56 +0500

    logging user in after sign up

--- lesson156/alpha-blog/app/controllers/users_controller.rb
+++ lesson156/alpha-blog/app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
