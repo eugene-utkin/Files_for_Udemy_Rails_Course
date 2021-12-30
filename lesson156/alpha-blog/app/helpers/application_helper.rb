cddc8db869f4ae2ae7853692cc4a62190a1ca5de
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-30 18:06:23 +0500

    current_user check added to current_user method

--- lesson156/alpha-blog/app/helpers/application_helper.rb
+++ lesson156/alpha-blog/app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
