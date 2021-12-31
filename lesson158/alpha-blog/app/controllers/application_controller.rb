80c179ddb47069f2162b443317df564ddcaaf176
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-31 20:50:55 +0500

    helper_method :current_user added to application controller

--- lesson158/alpha-blog/app/controllers/application_controller.rb
+++ lesson158/alpha-blog/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
-class ApplicationController < ActionController::Base  
+class ApplicationController < ActionController::Base
+
+    helper_method :current_user
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
