4507d5d74b631d88818b99e73f714e311db8258c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-31 20:29:03 +0500

    before_action :set_user, only: [:update]

--- lesson156/alpha-blog/app/controllers/users_controller.rb
+++ lesson156/alpha-blog/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
