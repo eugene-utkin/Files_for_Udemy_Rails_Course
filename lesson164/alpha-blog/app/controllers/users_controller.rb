6ad9da20e593fcff103ac04af4ca9fcb2f840d25
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-11 23:49:30 +0500

    start of Lesson 164

---
+++ lesson164/alpha-blog/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+      @user = User.find(params[:id])
+  end
+
+end
