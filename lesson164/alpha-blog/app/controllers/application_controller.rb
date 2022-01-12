afb83161b9f11452b99c92191c3e6382415dd580
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-12 18:49:57 +0500

    require_user method modified

--- lesson164/alpha-blog/app/controllers/application_controller.rb
+++ lesson164/alpha-blog/app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < ActionController::Base
     end
 
     def require_user
-
+      if !logged_in?
+        flash[:alert] = "You must be logged in to perform that action"
+        redirect_to login_path
+      end
     end
 
 end
