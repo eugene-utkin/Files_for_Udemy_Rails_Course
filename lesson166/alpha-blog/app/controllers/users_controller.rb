d16ccf9ea622e1c9def33c85bc9c6750b9605af7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-13 18:52:42 +0500

    :require_user added to user controller before_action

--- lesson166/alpha-blog/app/controllers/users_controller.rb
+++ lesson166/alpha-blog/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
