06c138ffa4e95fe1644c3769c2dd999756c18788
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-24 17:29:44 +0500

    Category model created

--- lesson178/alpha-blog/test/models/category_test.rb
+++ lesson178/alpha-blog/test/models/category_test.rb
@@ -2,4 +2,9 @@
 
   class CategoryTest < ActiveSupport::TestCase
 
+    test "category should be valid" do
+      @category = Category.new(name: "Sports")
+      assert @category.valid?
+    end
+
   end
