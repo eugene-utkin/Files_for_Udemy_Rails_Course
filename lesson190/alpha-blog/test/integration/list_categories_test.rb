2f3707b7d4097e1209058160a4786b16a5a8cdd0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-01 17:39:59 +0500

    categories index view created

--- lesson190/alpha-blog/test/integration/list_categories_test.rb
+++ lesson190/alpha-blog/test/integration/list_categories_test.rb
@@ -5,4 +5,10 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     @category = Category.create(name: "Sports")
     @category2 = Category.create(name: "Travel")
   end
+
+  test "should show categories listing" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
 end
