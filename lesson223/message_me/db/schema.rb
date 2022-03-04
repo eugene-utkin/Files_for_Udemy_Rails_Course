49e76ef35368e25ddf70cb5495a7d5042743770f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-04 21:23:24 +0500

    create_messages migration added to project

--- lesson223/message_me/db/schema.rb
+++ lesson223/message_me/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_160437) do
+ActiveRecord::Schema.define(version: 2022_03_04_162146) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
