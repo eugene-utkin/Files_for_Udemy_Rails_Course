d8011220e81ffc5f531143e853b837c1592fecc5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-08 18:06:42 +0500

    flashing error message on wrong login info

--- lesson227/message_me/app/controllers/sessions_controller.rb
+++ lesson227/message_me/app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+    end
   end
 
 end
