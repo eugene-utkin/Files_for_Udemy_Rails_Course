0b598deebdf80fd5a008f9f92f32c82e9c41c1e6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-10 20:04:37 +0500

    logged_in_redirect private method added

--- lesson229/message_me/app/controllers/sessions_controller.rb
+++ lesson229/message_me/app/controllers/sessions_controller.rb
@@ -21,4 +21,13 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
+
 end
