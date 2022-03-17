ba7e7f770f0c49e597e544babcb08fb6fa72b7e1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-17 20:58:14 +0500

    message input is cleaned

--- lesson237/message_me/app/assets/javascripts/application.js
+++ lesson237/message_me/app/assets/javascripts/application.js
@@ -27,9 +27,10 @@ submit_message = function() {
   $('#message_body').on('keydown', function(e) {
     if (e.keyCode == 13) {
       $('button').click();
+      e.target.value = "";
     };
-  })
-}
+  });
+};
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
