577d4c8f2f2fba7b803cb259ae5a1a84c6636c14
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-01 21:56:03 +0500

    end of lesson 255

--- lesson255/finance-tracker/db/schema.rb
+++ lesson255/finance-tracker/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_173431) do
+ActiveRecord::Schema.define(version: 2022_04_01_164822) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
