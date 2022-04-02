16d1a6caab0d8b39db2fb4d3294d6fc1510e3cb1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-02 13:26:06 +0500

    client variable added to new_lookup method"

--- lesson256/finance-tracker/app/models/stock.rb
+++ lesson256/finance-tracker/app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    
+    client = IEX::Api::Client.new(publishable_token: 'token',
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
   end
 end
