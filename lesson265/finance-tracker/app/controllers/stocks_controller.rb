f6aae30e16f0f7eb898aea047930280b84bb2e86
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-11 17:36:17 +0500

    rendering results through js

--- lesson265/finance-tracker/app/controllers/stocks_controller.rb
+++ lesson265/finance-tracker/app/controllers/stocks_controller.rb
@@ -5,9 +5,8 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         respond_to do |format|
-          format.js { render partial: 'users/results' }
+          format.js { render partial: 'users/result' }
         end
-        render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
