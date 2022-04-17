eb58d61ad5d41f27c1282859852a46b2d84b4edd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-17 18:42:40 +0500

    check_db method added

--- lesson271/finance-tracker/app/controllers/user_stocks_controller.rb
+++ lesson271/finance-tracker/app/controllers/user_stocks_controller.rb
@@ -1,2 +1,10 @@
 class UserStocksController < ApplicationController
+
+  def create
+
+    stock = Stock.new_lookup(params[:ticker])
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+  end
+
+
 end
