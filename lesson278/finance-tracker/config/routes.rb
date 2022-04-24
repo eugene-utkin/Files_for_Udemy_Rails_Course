eaf2aa981bf9715325ff2808b9568b7650efd9d7
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-24 10:05:18 +0500

    my_friends route added

--- lesson278/finance-tracker/config/routes.rb
+++ lesson278/finance-tracker/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
 end
