ab10f217c5b05ffe4155b02b22086c617dce8310
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-24 15:00:00 +0500

    start of Lesson 279

---
+++ lesson279/finance-tracker/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
+  devise_for :users
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'friends#search'
+end
