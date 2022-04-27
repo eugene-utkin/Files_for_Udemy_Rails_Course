aad771a385995bae580c48620a968459b6a3fb29
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-27 22:27:23 +0500

    start of Lesson 286

---
+++ lesson286/finance-tracker/app/helpers/friendshiot_helper.rb
@@ -0,0 +1,2 @@
+module FriendshiotHelper
+end
---
+++ lesson286/finance-tracker/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
+  devise_for :users
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+end
