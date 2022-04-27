4eb44c692d15207d21c096f9f4a109952de340b8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-27 22:04:14 +0500

    end of lesson 285

--- lesson285/finance-tracker/app/controllers/users_controller.rb
+++ lesson285/finance-tracker/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
