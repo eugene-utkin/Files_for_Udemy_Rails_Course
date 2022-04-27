55645ad47724890210d4294563e32e7f060852c8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-27 21:18:03 +0500

    user show added to routes

--- lesson285/finance-tracker/config/routes.rb
+++ lesson285/finance-tracker/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
