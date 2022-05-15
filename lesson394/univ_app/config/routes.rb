f5de767d9656e285890bafc03803049c41de102d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-15 19:13:06 +0500

    students controller added

--- lesson394/univ_app/config/routes.rb
+++ lesson394/univ_app/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
-  resources :students
+  resources :students, except: [:destroy]
 end
