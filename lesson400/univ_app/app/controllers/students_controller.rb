cd42aaca24300d4bff185d13e7b2809aa4dc7465
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-21 16:57:01 +0500

    start of Lesson 400

---
+++ lesson400/univ_app/app/controllers/students_controller.rb
@@ -0,0 +1,44 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
+end
