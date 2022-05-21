164d0a2a0d1f27bbc4ceac6094755caa8f82df36
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-21 17:34:17 +0500

    header and button condition added to student form

--- lesson400/univ_app/app/controllers/students_controller.rb
+++ lesson400/univ_app/app/controllers/students_controller.rb
@@ -17,7 +17,7 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
