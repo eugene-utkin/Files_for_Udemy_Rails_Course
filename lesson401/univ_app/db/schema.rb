17afb4e49b463c8805e30922f384fb241a8c7b29
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-22 17:09:47 +0500

    add password migration added

--- lesson401/univ_app/db/schema.rb
+++ lesson401/univ_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_124024) do
+ActiveRecord::Schema.define(version: 2022_05_22_120820) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_05_14_124024) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
