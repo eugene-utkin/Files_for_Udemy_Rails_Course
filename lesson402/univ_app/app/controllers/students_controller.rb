83c55e1e5f79ee1d718acb3b92544c7c813f85ca
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-23 19:27:50 +0500

    password added to permited params

--- lesson402/univ_app/app/controllers/students_controller.rb
+++ lesson402/univ_app/app/controllers/students_controller.rb
@@ -43,7 +43,7 @@ class StudentsController < ApplicationController
   end
 
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
