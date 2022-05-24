4a2901f03fd046cdade86d9bddbaeed80148d3e0
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-24 20:11:44 +0500

    login and logout routes created

--- lesson403/univ_app/config/routes.rb
+++ lesson403/univ_app/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  get 'login', to: 'logins#new'
+  post 'login', to: 'login#create'
+  delete 'logout', to: 'login#destroy'
   resources :students, except: [:destroy]
 end
