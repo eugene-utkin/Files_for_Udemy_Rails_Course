62fe347e6681626a5f33e2d2639f1d6f8c2e2247
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-25 20:56:57 +0500

    login create method updated

--- lesson404/univ_app/app/controllers/logins_controller.rb
+++ lesson404/univ_app/app/controllers/logins_controller.rb
@@ -5,7 +5,15 @@ class LoginsController < ApplicationController
   end
 
   def create
-
+    student = Student.find_by(email: params[:logins][:email].downcase)
+    if student && student.authenticate(params[:logins][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "You have successfully logged in"
+      redirect_to student
+    else
+      flash.now[:notice] = "Something was wrong with your login information"
+      render 'new'
+    end
   end
 
   def destroy
