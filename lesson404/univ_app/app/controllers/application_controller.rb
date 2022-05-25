aa53fe8465128a630268aaa34754d0ea42ec3622
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-25 21:08:59 +0500

    logged_in? and current_user methods added

--- lesson404/univ_app/app/controllers/application_controller.rb
+++ lesson404/univ_app/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user = Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
