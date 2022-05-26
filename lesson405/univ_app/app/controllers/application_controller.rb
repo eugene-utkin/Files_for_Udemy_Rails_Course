18f75cd3301a8d45c0a259681b06d19fff577011
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-26 17:58:58 +0500

    before_action require_user

--- lesson405/univ_app/app/controllers/application_controller.rb
+++ lesson405/univ_app/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
+  before_action :require_user
+
   def current_user
     @current_user ||= Student.find(session[:student_id]) if session[:student_id]
   end
