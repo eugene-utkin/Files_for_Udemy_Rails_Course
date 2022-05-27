39db1258c2ee87ec6c2180b478fc06d801e8630a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-27 20:33:39 +0500

    timestamps added to create_student_courses migration

--- lesson407/univ_app/db/migrate/20220527153020_create_student_courses.rb
+++ lesson407/univ_app/db/migrate/20220527153020_create_student_courses.rb
@@ -3,6 +3,7 @@ class CreateStudentCourses < ActiveRecord::Migration[5.2]
     create_table :student_courses do |t|
       t.integer :student_id
       t.integer :course_id
+      t.timestamps
     end
   end
 end
