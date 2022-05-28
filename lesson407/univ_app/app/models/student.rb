daa51def350a016d4d83bf2697af4913798aa121
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-28 17:34:42 +0500

    has_many student_courses added to student

--- lesson407/univ_app/app/models/student.rb
+++ lesson407/univ_app/app/models/student.rb
@@ -7,4 +7,5 @@ class Student < ApplicationRecord
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  has_many :student_courses
 end
