403f609b3b1504b89277f4537906ca6468b99554
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-05-30 22:49:22 +0500

    error notice added to create method of student_courses_controller

--- lesson408/univ_app/app/controllers/student_courses_controller.rb
+++ lesson408/univ_app/app/controllers/student_courses_controller.rb
@@ -2,6 +2,11 @@ class StudentCoursesController < ApplicationController
 
   def create
     course_to_add = Course.find(params[:course_id])
+    unless current_user.courses.include?(course_to_add)
+
+    else
+      flash[:notice] = "Something was wrong with your enrollment"
+    end
   end
 
 end
